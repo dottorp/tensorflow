a558dce6af25b2f9e02c9b6db0a93650d44b703b
Paris Pierpaolo <paris@eurisnet.it>
2018-10-30 17:17:50 +0100

    Test

--- tensorflow/House_Price_Prediction.py
+++ tensorflow/House_Price_Prediction.py
@@ -9,3 +9,8 @@ house_size = np.random.randint(low=1000, high=3500, size=num_house)
 
 np.random.seed(42)
 house_price = house_size * 100.0 + np.random.randint(low=1000,high=70000, size=num_house) 
+
+plt.plot(house_size,house_price,"bx")
+plt.ylabel("Price")
+plt.xlabel("Size")
+plt.show()
